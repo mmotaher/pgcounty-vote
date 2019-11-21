031d78a372d51afd5af71c29e55e3a8295193126
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2019-11-21 13:29:47 -0500

    added the server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('src'));
+
+// this is a single route, in the simplest possible format
+// the simplest format is not necessarily the best one.
+// this is, right now, an introduction to Callback Hell
+// but it is okay for a first-level example
+app.get('/api', (req, res) => {
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/2v6d-7p4w.json';
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/error');
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
