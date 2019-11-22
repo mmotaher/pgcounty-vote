95ece9d60b43c62f24319adebb310a746e547025
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2019-11-21 23:49:38 -0500

    Set the configuration to start on home page for the server

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const path = require('path');
+const engine = require('consolidate');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'src/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('src', {index: './src/pages/home_page.html'}));
+app.use(
+  express.static(path.join(__dirname, 'src'), {
+    index: path.join(__dirname, 'src/views/home_page.html')
+  })
+);
 
 // this is a single route, in the simplest possible format
 // the simplest format is not necessarily the best one.
 // this is, right now, an introduction to Callback Hell
 // but it is okay for a first-level example
 app.get('/api', (req, res) => {
-  const baseURL = 'https://data.princegeorgescountymd.gov/resource/2v6d-7p4w.json';
+  const baseURL =
+    'https://data.princegeorgescountymd.gov/resource/2v6d-7p4w.json';
   fetch(baseURL)
-    .then((r) => r.json())
-    .then((data) => {
+    .then(r => r.json())
+    .then(data => {
       console.log(data);
       res.send({ data: data });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.redirect('/error');
     });
+});
+
+app.get('/', function(req, res){
+  res.status(200).sendFile(path.join(__dirname, '/src/views/home_page.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
