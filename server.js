75e4e58fff4c77085d573c21d32437f00ee829dc
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2019-11-21 21:56:28 -0500

    making home-page.html the index

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('src'));
+app.use(express.static('src', {index: './src/pages/home_page.html'}));
 
 // this is a single route, in the simplest possible format
 // the simplest format is not necessarily the best one.
