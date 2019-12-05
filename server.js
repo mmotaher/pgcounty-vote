675974ded3d8bd5c7e8baf9fb6b62c2a45d3f28d
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2019-12-05 03:45:37 -0500

    removing unnecessary files

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const path = require('path');
 const engine = require('consolidate');
-const results = require('./routes/results');
 
-const url = 'https://data.princegeorgescountymd.gov/resource/2v6d-7p4w.json';
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -17,8 +14,5 @@ app.set('views', path.join(__dirname, 'build'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
-
-// Simple api request to PG Open Data
-app.use('/results', results);
 
 app.listen(port, console.log(`App is up and running on port ${port}!`));
